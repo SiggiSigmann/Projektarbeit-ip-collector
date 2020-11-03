2dd6fdc86c0ef2743178e5cd10f36089f4af2a25
Tobias Sigmann <siggisigmann@gmail.com>
2020-11-03 21:56:21 +0100

    debug

--- server/flaskServer/tracert.py
+++ server/flaskServer/tracert.py
@@ -19,35 +19,38 @@ class Tracert():
 
         for i in range(1, 28):
             print("thread: " + ip + " " + str(i), file=sys.stderr)
-            pkt = IP(dst=ip, ttl=i) / UDP(dport=33434)
-            # Send the packet and get a reply
-            reply = sr1(pkt, verbose=0)
-            if reply is None:
-                # No reply =(
-                print("thread: No reply " + ip , file=sys.stderr)
-                trace.append([i,"-", "-"])
-                
-            elif reply.type == 3:
-                # We've reached our destination
-                hostname = ""
-                try:
-                    hostname = socket.gethostbyaddr(reply.src)[0]
-                except:
-                    hostname = "-"
+            try:
+                pkt = IP(dst=ip, ttl=i) / UDP(dport=33434)
+                # Send the packet and get a reply
+                reply = sr1(pkt, verbose=0)
+                if reply is None:
+                    # No reply =(
+                    print("thread: No reply " + ip , file=sys.stderr)
+                    trace.append([i,"-", "-"])
+                    
+                elif reply.type == 3:
+                    # We've reached our destination
+                    hostname = ""
+                    try:
+                        hostname = socket.gethostbyaddr(reply.src)[0]
+                    except:
+                        hostname = "-"
 
-                print("thread: done " + ip , file=sys.stderr)
-                trace.append([i, reply.src, hostname])
-                
-                break
-            else:
-                hostname = ""
-                try:
-                    hostname = socket.gethostbyaddr(reply.src)[0]
-                except:
-                    hostname = "-"
+                    print("thread: done " + ip , file=sys.stderr)
+                    trace.append([i, reply.src, hostname])
+                    
+                    break
+                else:
+                    hostname = ""
+                    try:
+                        hostname = socket.gethostbyaddr(reply.src)[0]
+                    except:
+                        hostname = "-"
 
-                trace.append([i, reply.src, hostname])
-                print("thread: weiter " + ip , file=sys.stderr)
+                    trace.append([i, reply.src, hostname])
+                    print("thread: weiter " + ip , file=sys.stderr)
+            except:
+                print("error " + ip , file=sys.stderr)
 
                 
         print("done with trace: " + ip, file=sys.stderr)
