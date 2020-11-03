9bee38126ffd788cf9c4f87b600a9919f32c1084
Tobias Sigmann <siggisigmann@gmail.com>
2020-11-03 15:59:45 +0100

    change tracert method

--- server/flaskServer/server.py
+++ server/flaskServer/server.py
@@ -6,48 +6,9 @@ import os
 import dbconnector.dbconnector as dbcon
 import socket
 import tracert as tr
-from scapy.all import *
-import sys
+from icmplib import ping, multiping, traceroute, Host, Hop
 
-
-print("start trace", file=sys.stderr)
-trace = []
-for i in range(1, 28):
-    print("dst="+str("www.google.de")+", ttl="+str(i), file=sys.stderr)
-    pkt = IP(dst=str("www.google.de"), ttl=i) / UDP(dport=33434)
-    print(pkt, file=sys.stderr)
-    # Send the packet and get a reply
-    reply = sr1(pkt, verbose=0)
-    print(reply, file=sys.stderr)
-    if reply is None:
-        # No reply =(
-        
-        trace.append([i,"-", "notfound"])
-        print("--> " + str(i) + " -" , file=sys.stderr) 
-
-    elif reply.type == 3:
-        # We've reached our destination
-        try:
-            trace.append([i,reply.src, socket.gethostbyaddr(reply.src)])
-            print("--> " + str(i) + " " + reply.src, file=sys.stderr) 
-        except:
-            trace.append([i,reply.src, "notfound"])
-            print("--> " + str(i) + " " + reply.src, file=sys.stderr)
-        break
-    else:
-    # We're in the middle somewhere
-        # We've reached our destination
-        try:
-            trace.append([i,reply.src, socket.gethostbyaddr(reply.src)])
-            print("--> " + str(i) + " " + reply.src, file=sys.stderr) 
-        except:
-            trace.append([i,reply.src, "notfound"])
-            print("--> " + str(i) + " " + reply.src, file=sys.stderr)
-        break
-
-print(trace, file=sys.stderr)
-print("stop trace", file=sys.stderr)
-
+print(traceroute("www.google.de"))
 
 datadb = dbcon.dbconnector(socket.gethostbyname('db'),"networkdata", "test", "1234567")#
 datadb.select()
--- server/flaskServer/tracert.py
+++ server/flaskServer/tracert.py
@@ -1,7 +1,6 @@
-from scapy.all import *
+from icmplib import ping, multiping, traceroute, Host, Hop
 import threading
 import dbconnector.dbconnector as dbcon
-import sys
 
 class Tracert():
     def __init__(self, db):
@@ -12,32 +11,8 @@ class Tracert():
         x.start()
 
     def run(self, ip, traceId):
-        trace = []
         print("start trace", file=sys.stderr)
-        
-        for i in range(1, 28):
-            print("dst="+str(ip)+", ttl="+str(i), file=sys.stderr)
-            pkt = IP(dst=ip, ttl=i) / UDP(dport=33434)
-            print(pkt, file=sys.stderr)
-            # Send the packet and get a reply
-            reply = sr1(pkt, verbose=0)
-            print(reply, file=sys.stderr)
-            if reply is None:
-                # No reply =(
-                
-                trace.append([i,"-"])
-                print("--> " + str(i) + " -" , file=sys.stderr) 
-            elif reply.type == 3:
-                # We've reached our destination
-                trace.append([i,reply.src])
-                print("--> " + str(i) + " " + reply.src, file=sys.stderr) 
-                break
-            else:
-            # We're in the middle somewhere
-                trace.append([i,reply.src])
-                print("--> " + str(i) + " " + reply.src, file=sys.stderr) 
-
-
+        trace = traceroute(ip)
         print("stop trace", file=sys.stderr)
         self.datadb.insertTrace(traceId, trace)
 
