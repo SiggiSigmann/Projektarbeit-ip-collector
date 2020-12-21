fbb11efb00745d03bef368b06e58a701dd2a61d3
Tobias Sigmann <siggisigmann@gmail.com>
2020-12-21 13:50:07 +0100

    last bugfix for now

--- server/flaskServer/plotter.py
+++ server/flaskServer/plotter.py
@@ -14,15 +14,15 @@ class Plotter():
         self.sub = Subnetze("/files/de.csv")
 
     def get_Json(self, user):
-        return json.loads('{"images":[{"url": "/image/'+user+'_0.png", "alt":"Hour", "height":400, "width":400, "description":"Shows distance between measurements"}, '+\
-                                     '{"url": "/image/'+user+'_1.png", "alt":"Day", "height":400, "width":400, "description":"Shows how many measurements where done per ay"}, '+\
-                                     '{"url": "/image/'+user+'_2.png", "alt":"IpAddresses", "height":400, "width":400, "description":"Shows distribution of IP-Adresses of the Users device"},'+\
-                                     '{"url": "/image/'+user+'_3.png", "alt":"IpAddresses in Trace", "height":400, "width":400, "description":"Shows distribution of IP-Adresses in Trace"},'+\
-                                     '{"url": "/image/'+user+'_4.png", "alt":"Subnet IP-Addresses", "height":400, "width":400, "description":"Show IP ownder duration"},'+\
-                                     '{"url": "/image/'+user+'_5.png", "alt":"Subnet IP-Addresses trace", "height":400, "width":400, "description":"Show IP ownder duration of trace"},'+\
-                                     #'{"url": "/image/'+user+'_6.png", "alt":"Subnet IP-Addresses", "height":400, "width":400, "description":"Show IP ownder duration"},'+\
-                                     #'{"url": "/image/'+user+'_7.png", "alt":"Subnet IP-Addresses trace", "height":400, "width":400, "description":"Show IP ownder duration of trace"}'+\
-                                     ']}')
+        return json.loads('{"images":[{"url": "/image/'+user+'_0.png", "alt":"Hour", "height":400, "width":400, "description":"Shows distance between measurements"} '+\
+                                    ',{"url": "/image/'+user+'_1.png", "alt":"Day", "height":400, "width":400, "description":"Shows how many measurements where done per ay"} '+\
+                                    ',{"url": "/image/'+user+'_2.png", "alt":"IpAddresses", "height":400, "width":400, "description":"Shows distribution of IP-Adresses of the Users device"}'+\
+                                    ',{"url": "/image/'+user+'_3.png", "alt":"IpAddresses in Trace", "height":400, "width":400, "description":"Shows distribution of IP-Adresses in Trace"}'+\
+                                    ',{"url": "/image/'+user+'_4.png", "alt":"Subnet IP-Addresses", "height":400, "width":400, "description":"Show IP ownder duration"}'+\
+                                    ',{"url": "/image/'+user+'_5.png", "alt":"Subnet IP-Addresses trace", "height":400, "width":400, "description":"Show IP ownder duration of trace"}'+\
+                                    #',{"url": "/image/'+user+'_6.png", "alt":"Subnet IP-Addresses", "height":400, "width":400, "description":"Show IP ownder duration"}'+\
+                                    #',{"url": "/image/'+user+'_7.png", "alt":"Subnet IP-Addresses trace", "height":400, "width":400, "description":"Show IP ownder duration of trace"}'+\
+                                    ']}')
 
     def _create_random_figure(self):
         fig = Figure()
