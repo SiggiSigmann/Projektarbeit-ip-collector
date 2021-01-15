221b3a04daa964a12b3abd7a3838056161e1fccf
Tobias Sigmann <siggisigmann@gmail.com>
2021-01-15 14:49:46 +0100

    done

--- server/backup/backup.sh
+++ server/backup/backup.sh
@@ -5,7 +5,7 @@ path_to_backup =  "/home/ubuntu/projektarbeit-iobased-login/server/backup"
 docker exec server_db_1 mysqldump -u root --password="1234567" networkdata > "${path_to_backup}/backup_$(date +"%Y_%m_%d_%I_%M_%p").sql"
 
 #delete older file
-find $path_to_backup -name "backup_*.sql" -type f -mtime +20 -depth -print
+find $path_to_backup -name "backup*.sql" -type f -mtime +20 -delete
 
 #load backupscript
 #cat backup_date.sql | docker exec -i server_db_1 mysql -u root --password=1234567 networkdata
