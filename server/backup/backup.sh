7c5258c07022bcc105722685f696bdb8e3ada732
Tobias Sigmann <siggisigmann@gmail.com>
2021-01-15 14:50:59 +0100

    done with backup

--- server/backup/backup.sh
+++ server/backup/backup.sh
@@ -6,6 +6,7 @@ docker exec server_db_1 mysqldump -u root --password="1234567" networkdata > "${
 
 #delete older file
 find $path_to_backup -name "backup*.sql" -type f -mtime +20 -delete
+find $path_to_backup -name "output*.txt" -type f -mtime +20 -delete
 
 #load backupscript
 #cat backup_date.sql | docker exec -i server_db_1 mysql -u root --password=1234567 networkdata
