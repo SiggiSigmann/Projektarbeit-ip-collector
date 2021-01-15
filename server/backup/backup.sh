f42cb9e95c52b4dffb09d4c6dbd37ea8a8e36e7a
Tobias Sigmann <siggisigmann@gmail.com>
2021-01-15 14:45:23 +0100

    imporve

--- server/backup/backup.sh
+++ server/backup/backup.sh
@@ -2,10 +2,10 @@
 path_to_backup =  "/home/ubuntu/projektarbeit-iobased-login/server/backup"
 
 #create backup
-docker exec server_db_1 mysqldump -u root --password="1234567" networkdata > "$(path_to_backup)/backup$(date +"%Y_%m_%d_%I_%M_%p").sql"
+docker exec server_db_1 mysqldump -u root --password="1234567" networkdata > "${path_to_backup}/backup_$(date +"%Y_%m_%d_%I_%M_%p").sql"
 
 #delete older file
-find . -name "($path_to_backup)/backup*.sql" -type f --mtime +5 -depth -print
+find $path_to_backup -name "backup_*.sql" -type f -mtime +20 -depth -print
 
 #load backupscript
 #cat backup_date.sql | docker exec -i server_db_1 mysql -u root --password=1234567 networkdata
