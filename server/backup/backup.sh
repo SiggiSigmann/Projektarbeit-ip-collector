111a49a64325f261afcbf40a77f301b5a4d96c6a
Tobias Sigmann <siggisigmann@gmail.com>
2021-01-15 14:32:59 +0100

    fix in backup file

--- server/backup/backup.sh
+++ server/backup/backup.sh
@@ -2,10 +2,10 @@
 path_to_backup =  "/home/ubuntu/projektarbeit-iobased-login/server/backup"
 
 #create backup
-docker exec server_db_1 mysqldump -u root --password="1234567" networkdata > "$path_to_backup/backup$(date +"%Y_%m_%d_%I_%M_%p").sql"
+docker exec server_db_1 mysqldump -u root --password="1234567" networkdata > "$(path_to_backup)/backup$(date +"%Y_%m_%d_%I_%M_%p").sql"
 
 #delete older file
-find /path/to/files* -mtime +5 -exec rm {} \;
+find . -name "($path_to_backup)/backup*.sql" -type f --mtime +5 -depth -print
 
 #load backupscript
 #cat backup_date.sql | docker exec -i server_db_1 mysql -u root --password=1234567 networkdata
