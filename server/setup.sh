2e841918bdd1a969fb7aa816a126adde54d73c24
Tobias Sigmann <siggisigmann@gmail.com>
2021-01-26 14:16:37 +0100

    change backup dir

--- server/setup.sh
+++ server/setup.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+cp -r ./backup/ /home/ubuntu/backup/
+
 #start backup
 #todo: change paths in chron.txt and backup.sh
-crontab ./backup/cron.txt
+crontab /home/ubuntu/backup/cron.txt
 #remove everything vrom chron ->crontab -r
+
+
 
 #start server
 chmod +x ./start.sh
